6b1a26f523f2909e2cf27ce1b6c634f4fac2424a
unknown <deep0072>
2022-08-28 17:59:52 +0530

    string operations

---
+++ Day7/string.js
@@ -0,0 +1,36 @@
+var str = "deepak"
+
+var a = str.length // get length
+
+// var a = str.toUpperCase() // convert in uppercase
+// var a = str.toLowerCase() // convert in Lowercase
+// var a = str.includes("Is") // check mentioned word give boolean
+// var a = str.search("Is") // check mentioned word give its index other wise -1
+// var a  = str.match(/word/g) // check mentioned word in string gives array
+// var a = str.startsWith("Dee") // gives boolean
+
+// var a = str.endsWith("pak") // true
+
+//====================================================================================
+
+//concatenate string
+// var str1 = "Deepak"
+// var str2 = " Kumar"
+// console.log(str1.concat(str2)) // Deepak Kumar
+
+// trim space on both side of string
+// var a =  " Deepak "
+// console.log(a.trim()) // Deepak
+
+// slice ==> iit slice the part of string by passing start and end points
+// var name = "Deepak Kumar"
+// console.log(name.slice(1,5)) // eepa
+
+//split ==> split the string by some character and return an array by default it split by comma
+var name = "Deepak Kumar"
+console.log(name.split(" "))  // [ 'Deepak', 'Kumar' ]
+
+// convert number to string
+var a = 90
+console.log(a.toString()) // "90"
+
