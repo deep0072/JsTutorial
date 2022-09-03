186273fc4c87e5dab793d875074e17db8e48d138
unknown <deep0072>
2022-09-03 17:50:13 +0530

    maths operation

---
+++ Day8/math.js
@@ -0,0 +1,35 @@
+// let result = Math.ceil(3.4) // ceil means it will give always upper value
+// console.log(result) // 4
+// let result = Math.floor(3.4) // floor means it will give always lower value
+// console.log(result) // 3
+
+// let result = Math.round(3.4) // round means it will give always lower value if value after . is lower than 5 and give higher if more than5
+// console.log(result) // 3
+// let result = Math.trunc(3.4) // trunc means it will give always lower value
+// console.log(result) // 3
+
+// //get max value
+// console.log(Math.max(7, 8, 46, 34, -1)); // out 46
+
+// //get min value
+// console.log(Math.min(7, 8, 46, 34, -1)); // out -1
+
+//get sqrt
+// console.log(Math.sqrt(625)) // 25
+
+//cube root
+// console.log(Math.cbrt(125)) // 5
+
+// power
+// console.log(Math.pow(4,2)) // 4*4 = 16
+
+// get +ve value
+// console.log(Math.abs(-5.5)) // negative to positive value ==> 5.5
+
+//get pi value
+// console.log(Math.PI) //3.141592653589793
+
+// get random value 
+console.log(Math.random()) // by defualt give value btw 0 to one like 0.95 or something else
+
+
