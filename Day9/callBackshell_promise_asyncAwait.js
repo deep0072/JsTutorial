b5f92fe3147a98604e55caa2641f7366f308d465
deep0072 <deepakumar50136@gmail.com>
2022-10-29 19:17:46 +0530

    call back hell, promises,async and await

---
+++ Day9/callBackshell_promise_asyncAwait.js
@@ -0,0 +1,74 @@
+/*
+ this is call back hell multiple call back called internally
+
+*/
+
+let promise = () => {
+  setTimeout(() => {
+    console.log("boil water"); // wait for 10 second then print this line
+    setTimeout(() => {
+      console.log("add kofee powder"); // after 10 second this function is called wait for 2 second
+      setTimeout(() => {
+        console.log("add sugar as per taste"); // then wait for one second this line print
+        setTimeout(() => {
+          console.log("serve coffe");
+        });
+      }, 1000);
+    }, 2000);
+  }, 10000);
+};
+
+promise();
+
+//--------------------------------------to avoid call back hell use Promise
+
+let promise = (time, work) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`${work}   ${time} ms`);
+    }, time);
+  });
+};
+
+promise(10000, "boil water")
+  .then((data) => {
+    console.log(data);
+    return promise(2000, "Add coffee water and let remain it on flame ");
+  })
+  .then((data) => {
+    console.log(data);
+    return promise(1000, "add sugar and let remain it  on flame");
+  })
+  .then((data) => {
+    console.log(data);
+    return promise(0000, "serve coffee");
+  })
+  .then((data) => {
+    console.log(data);
+  });
+
+//-----------------------------------------------------------------------
+// // now to remove headace of calling then keyword multiple time  etc. use async await. it make code more cleaner
+let promise = (time, work) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`${work}   ${time} ms`);
+    }, time);
+  });
+};
+
+const caller = async () => {
+  let step1 = await promise(1000, "boil water");
+  console.log(step1);
+  let step2 = await promise(
+    2000,
+    "Add coffee water and let remain it on flame "
+  );
+  console.log(step2);
+  let step3 = await promise(1000, "add sugar and let remain it  on flame");
+  console.log(step3);
+  let step4 = await promise(0, "serve coffee");
+  console.log(step4);
+};
+
+caller();
