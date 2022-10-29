8df279746aa2e7a050adae1a7cc7893097f1e5e8
deep0072 <deepakumar50136@gmail.com>
2022-10-29 14:39:56 +0530

    promise example

--- Day9/promise.js
+++ Day9/promise.js
@@ -8,3 +8,21 @@ const promise = new Promise((resolve, reject) => {
   .catch((err) => {
     console.log(err); //value will be equal to the argument is givene in reject method
   });
+
+const fs = require("fs");
+
+new Promise((resolve, reject) => {
+  fs.readFile("deep.txt", (err, data) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(data.toString());
+    }
+  });
+})
+  .then((value) => {
+    console.log(value); // value is refer to the params of resolve(data.toString())
+  })
+  .catch((err) => {
+    console.log(err); // err ==> refer to 'reject' containing the 'err' as keyword reject(err)
+  });
